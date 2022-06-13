64e6f510df3356eeff2893db71d130c39b154008
Lisa <92580887+lisahns@users.noreply.github.com>
2022-06-13 18:52:02 +0100

    Update script.js
    add API Key

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+const apiKey = 24c5b19a49cfefbc4da219de97474cb3
 
+
+// Example API Request
+// https://api.themoviedb.org/3/movie/550?api_key=24c5b19a49cfefbc4da219de97474cb3
+
+// API Read Access Token (v4 auth)
+//eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyNGM1YjE5YTQ5Y2ZlZmJjNGRhMjE5ZGU5NzQ3NGNiMyIsInN1YiI6IjYyYTc3NWZkM2UyZWM4MDA5YmMwN2RjNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.x4D22BLP3HHo2U9wp-1Q8imZQh--rXCf_2X2oV30jfo
