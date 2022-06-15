74856add4331bca087eff64650a2d0ccdbd37077
flare-s <webflarez@gmail.com>
2022-06-15 15:39:11 +0100

    Separating the main branch from the other content

--- script.js
+++ script.js
@@ -9,36 +9,3 @@ const giphyApi = "rGUKmT78evm9GztNgAdrUuRuYUOJ2ZXO"
 
 
 
-const createMovies = (movies) => {
-    movies.results.forEach(result => {
-        let movieItem = document.createElement("li");
-        let title = document.createElement("h3");
-        title.textContent = result.title;
-        let img = document.createElement("img");
-        img.setAttribute("src", `https://image.tmdb.org/t/p/w500${result.poster_path}`);
-        img.setAttribute("alt", `${result.title} poster`);
-        let overview = document.createElement("p");
-        overview.textContent = result.overview;
-        movieItem.append(title, img, overview);
-        moviesList.append(movieItem);
-    });
-}
-
-
-const fetchRequest = (url) => {
-    return fetch(url)
-            .then(response => {
-                if (response.ok) {
-                    return response.json();
-                }
-                throw new Error('Something went wrong with request code: ' + response.status);
-            })
-}
-
-
-fetchRequest(`https://api.themoviedb.org/3/movie/popular?api_key=${movieApi}&language=en-US&page=1`)
-    .then(movies =>  {
-        console.log(movies);
-        createMovies(movies)
-    })
-    .catch(error => console.log(error))
