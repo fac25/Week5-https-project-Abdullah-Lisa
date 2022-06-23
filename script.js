5407039f22f3990ead3af1b2815f096d5a0f0d0b
flare-s <webflarez@gmail.com>
2022-06-23 14:06:22 +0100

    Refactored some code

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const giphyUrl = `https://api.giphy.com/v1/gifs/search`;
 const logo = document.querySelector(".logo");
 const randomSection = document.querySelector(".movies");
 const moviesList = document.querySelector(".movies-list");
+const errorSection = document.querySelector(".error");
 const errorMessage = document.querySelector(".error-message");
 
 let movieSection = document.querySelector(".movie");
@@ -25,14 +26,7 @@ const handleGetMovie = (e, id) => {
 const getMovie = (id) => {
     return Promise.all([fetchRequest(`https://api.themoviedb.org/3/movie/${id}?api_key=${movieApi}&language=en-US`), fetchRequest(configUrl)])
         .then(results => createMovie(results))
-        .catch(error => {
-            console.log(error);
-            errorMessage.textContent = "Sorry, something went wrong. Please try to refresh the page or visit us later";
-            movieSection.classList.add("hidden");
-            randomSection.classList.add("hidden");
-            errorMessage.classList.remove("hidden");
-            errorMessage.classList.add("show");
-        })
+        .catch(error => handleError(error, "Sorry, something went wrong. Please try to refresh the page or visit us later"))
 }
 
 
@@ -42,11 +36,9 @@ const createMovie = (results) => {
     let genresString = ``;
     details.genres.forEach(genre => genresString += `<li>${genre.name}</li>`);
             randomSection.classList.add("hidden");
+            let imgSrc = details.poster_path ? `${config.images.secure_base_url}w500${details.poster_path}` : "./images/placeholder.jpeg";
             let content = `
                 <div class="container">
-                    <div class="button-container">
-                      <button class="movies-button">Back to movies</button>
-                    </div>
                     <div class="movie-grid">
                     <div class="movie-content vertical-gutter">
                         <h2>${details.title}</h2>
@@ -61,24 +53,13 @@ const createMovie = (results) => {
                         <p class="movie-language">Language: <span>${details.original_language.toUpperCase()}.</span></p>
                     </div>
                     <div class="movie-img">
-                        <img class="img" src="${config.images.secure_base_url}w500${details.poster_path}" width="100" alt="${details.title} poster""/>
+                        <img class="img" src="${imgSrc}" width="300" height="400" alt="${details.title} poster""/>
                     </div>
                     </div>
                 </div>
                 `
                 movieSection.innerHTML = content;
-                movieSection.querySelector(".movies-button").addEventListener("click", () => {
-                    createMoviesList(randomMoviesUrl + `&page=${Math.floor(Math.random() * 50)}`)
-                        .then(title => title.textContent = "Popular Movies")
-                        .catch(error => {
-                            console.log(error);
-                            errorMessage.textContent = "Sorry, something went wrong. Please try to refresh the page or visit us later";
-                            movieSection.classList.add("hidden");
-                            randomSection.classList.add("hidden");
-                            errorMessage.classList.remove("hidden");
-                            errorMessage.classList.add("show");
-                        })
-                })
+                createBackButton(movieSection.querySelector(".container"), "afterbegin");
                 movieSection.classList.remove("hidden");
                 movieSection.classList.add("show");
                 fetchRequest(`${giphyUrl}?q=${details.title}&api_key=${giphyApi}&limit=1&rating=g`)
@@ -87,6 +68,8 @@ const createMovie = (results) => {
                     let giphy = document.createElement("img");
                     giphy.src = result.data[0].images.fixed_width.url;
                     giphy.alt = result.data[0].title;
+                    giphy.height = "200";
+                    giphy.width = "250";
                     content.append(giphy);
                   })
                   .catch(error => console.log(error));
@@ -94,11 +77,7 @@ const createMovie = (results) => {
 
 const createMovies = (data) => {
     if (data[2].results.length === 0) {
-        randomSection.classList.add("hidden");
-        movieSection.classList.add("hidden");
-        errorMessage.textContent = `Sorry, we couldn't find what you're looking for.`;
-        errorMessage.classList.remove("hidden");
-        errorMessage.classList.add("show");
+        handleError(null, `Sorry, we couldn't find what you're looking for.`);
     } else {
         let items = '';
         data[2].results.forEach(result => {
@@ -112,6 +91,7 @@ const createMovies = (data) => {
             } 
             let genresString = '';
             genres.forEach(el => genresString += `<li>${el.name}</li>`);
+            let imgSrc = result.poster_path ? `${data[0].images.secure_base_url}w500${result.poster_path}` : "./images/placeholder.jpeg";
             let itemContent = `
                 <li>
                     <a href="#" data-id="${result.id}" class="vertical-gutter movie-link">
@@ -121,7 +101,7 @@ const createMovies = (data) => {
                                 ${genresString}
                             </ul>
                         </div>
-                        <img src="${data[0].images.secure_base_url}w500${result.poster_path}" alt="${result.title} poster"/>
+                        <img src="${imgSrc}" width="300" height="400" alt="${result.title} poster"/>
                     </a>
                 </li>
             `;
@@ -129,7 +109,7 @@ const createMovies = (data) => {
         });
         moviesList.innerHTML = items;
         movieSection.classList.add("hidden");
-        errorMessage.classList.add("hidden");
+        errorSection.classList.add("hidden");
         randomSection.classList.remove("hidden");
         randomSection.classList.add("show");
         const movieLinks = document.querySelectorAll(".movie-link");
@@ -160,52 +140,54 @@ logo.addEventListener("click", (e) => {
     e.preventDefault;
     createMoviesList(randomMoviesUrl + `&page=${Math.floor(Math.random() * 50)}`)
     .then(title => title.textContent = "Popular Movies")
-    .catch(error => {
-        console.log(error);
-        errorMessage.textContent = "Sorry, something went wrong. Please try to refresh the page or visit us later";
-        movieSection.classList.add("hidden");
-        randomSection.classList.add("hidden");
-        errorMessage.classList.remove("hidden");
-        errorMessage.classList.add("show");
-    })
+    .catch(error => handleError(error, "Sorry, something went wrong. Please try to refresh the page or visit us later"))
 })
 
 
 
 createMoviesList(randomMoviesUrl + `&page=${Math.floor(Math.random() * 50)}`)
     .then(title => title.textContent = "Popular Movies")
-    .catch(error => {
-        console.log(error);
-        errorMessage.textContent = "Sorry, something went wrong. Please try to refresh the page or visit us later";
-        movieSection.classList.add("hidden");
-        randomSection.classList.add("hidden");
-        errorMessage.classList.remove("hidden");
-        errorMessage.classList.add("show");
-    })
+    .catch(error => handleError(error, "Sorry, something went wrong. Please try to refresh the page or visit us later"));
 
 // search movies
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    const search = document.getElementById("movie-name").value;
-    if (search.trim() === "") {
-        errorMessage.textContent = "Please don't leave the search field empty.";
-        movieSection.classList.add("hidden");
-        randomSection.classList.add("hidden");
-        errorMessage.classList.remove("hidden");
-        errorMessage.classList.add("show");
+    const search = document.getElementById("movie-name");
+    let searchVal = search.value;
+    if (searchVal.trim() === "") {
+        handleError(null, "Please don't leave the search field empty.");
         return;
     }
 
-    createMoviesList(searchURL + `&query=${search}`)
-      .then(title => title.textContent = search)
-      .catch(error => {
-        console.log(error);
-        errorMessage.textContent = "Sorry, something went wrong. Please try to refresh the page or visit us later";
-        movieSection.classList.add("hidden");
-        randomSection.classList.add("hidden");
-        errorMessage.classList.remove("hidden");
-        errorMessage.classList.add("show");
-    })
+    createMoviesList(searchURL + `&query=${searchVal}`)
+      .then(title => title.textContent = `Results for "${searchVal}"`)
+      .catch(error => handleError(error, "Sorry, something went wrong. Please try to refresh the page or visit us later"))
+    search.value = "";
 });
 
+const createBackButton = (target, position) => {
+    let buttonContainer = document.createElement("div");
+    buttonContainer.classList.add(("button-container"));
+    let button = document.createElement("button");
+    button.textContent = "Back to movies";
+    button.classList.add("movies-button");
+    button.addEventListener("click", () => {
+        createMoviesList(randomMoviesUrl + `&page=${Math.floor(Math.random() * 50)}`)
+            .then(title => title.textContent = "Popular Movies")
+            .catch(error => handleError(error, "Sorry, something went wrong. Please try to refresh the page or visit us later"))
+    });
+    buttonContainer.append(button);
+    target.insertAdjacentElement(position, buttonContainer);
+
+}
+
+const handleError = (error, message) => {
+    console.log(error);
+    errorMessage.textContent = message;
+    createBackButton(errorMessage, "beforeend");
+    movieSection.classList.add("hidden");
+    randomSection.classList.add("hidden");
+    errorSection.classList.remove("hidden");
+    errorSection.classList.add("show");
+}
 
